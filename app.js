4f7e47392404802b5326dfe615b6233b91f60d03
Kareem <kpjackson27@hotmail.com>
2015-05-07 00:22:38 -0400

    refactor

--- app.js
+++ app.js
@@ -2,13 +2,32 @@
 
 angular.module('madLibs',[])
 	.controller('MainCtrl',function ($scope){
-		$scope.name = '';
-		$scope.jtitle = '';
-		$scope.task1 = '';
-		$scope.task2 = '';
-		$scope.celebrity = '';	
-		$scope.skill1 = '';
-		$scope.celebrity2 = '';
-		$scope.number = '';
-		$scope.adjective = '';
+		
+		$scope.hideText = true;
+		$scope.showText = function(){
+			return $scope.hideText = false;
+		};
+
+		$scope.reset = function (){
+
+			if(!$scope.hideText){
+				$scope.malename = '';
+				$scope.jtitle = '';
+				$scope.task1 = '';
+				$scope.task2 = '';
+				$scope.celebrity = '';
+				$scope.celebrity2 = '';
+				$scope.task1 = '';
+				$scope.task2 = '';
+				$scope.skill1 = '';
+				$scope.number = '';
+				$scope.adjective = '';
+				$scope.hideText = true;
+			};
+
+			
+		};
+
 	});
+	
+
